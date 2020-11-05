66cd519a02620db3ee97eabe2eadd2441523758d
Ran Jun <ranjun@ybtcn.cn>
2020-11-05 15:19:52 +0800

    fix gitignore

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@
 set -e
 
 # 生成静态文件
-npm run docs:build
+yarn docs:build
 
 # 进入生成的文件夹
 cd docs/.vuepress/dist
@@ -16,10 +16,14 @@ git init
 git add -A
 git commit -m 'deploy'
 
+git config user.name CantFindItAnymore
+git config user.email 1069811427@qq.com
+
 # 如果发布到 https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
 git push -f git@github.com:CantFindItAnymore/myBlog.git master:gh-pages
+# git push -f https://github.com/CantFindItAnymore/myBlog.git master:gh-pages
 
 cd -
