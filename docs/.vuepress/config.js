e11e92510a19236c35e020ed94b9f48370d6f401
Ran Jun <ranjun@ybtcn.cn>
2020-11-05 11:30:17 +0800

    init

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,54 @@
+module.exports = {
+	title: "can\'t find it anymore",
+	description: '小哥哥小姐姐进来玩啊~(✿◡‿◡)',
+	head: [['link', { rel: 'icon', href: '/icon.png' }]],
+	port: 3000,
+	// markdown: {
+	// 	lineNumbers: false,
+  // },
+
+  themeConfig: {
+    lastUpdated: '最后更新时间',
+    sidebar: 'auto',
+    repo: 'https://github.com/CantFindItAnymore/myBlog',
+    repoLabel: 'Github',
+    nav: [
+      {
+        text: '前端面试梳理',
+        link: '/interview/'
+      },
+      {
+        text: 'TS',
+        link: '/ts/'
+      },
+      {
+        text: 'webpack',
+        link: '/webpack/'
+      },
+      {
+        text: '运维',
+        link: '/devops/'
+      },
+      {
+        text: 'node',
+        link: '/node/'
+      },
+      {
+        text: '实战',
+        link: '/actualCombat/'
+      }
+    ],
+    // sidebar: {
+    //   '/webpack/': [webpackSidebar],
+    //   '/vueAnalysis/': vueAnalysisSidebar
+    // }
+  },
+  configureWebpack: {
+    resolve: {
+      alias: {
+        '@images': '../images',
+        '@components': './components'
+      }
+    }
+  }
+}
---
+++ docs/README.md
@@ -0,0 +1,16 @@
+---
+home: true
+lang: zh-CN
+heroText:
+heroImage: /logo.png
+actionText: 开始 →
+actionLink: /interView/
+features:
+  - title: 这里是
+    details: 前端博客，全栈方向
+  - title: 对于我
+    details: 试试吧 不行就回老家养🐖
+  - title: 目前在
+    details: 成都 > hard working...
+footer: Copyright © 2020-present Ran Jun
+---
